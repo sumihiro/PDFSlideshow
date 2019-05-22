684544966a1aa689e0992e8fd7e46d61958b044b
sumihiro <ueda@sumihiro.biz>
2019-05-22 09:44:44 +0900

    状態監視とボタンの有効判定追加

--- PDFReader/ViewController.m
+++ PDFReader/ViewController.m
@@ -29,13 +29,33 @@
     NSURL *documentURL = [[NSBundle mainBundle] URLForResource:@"サンプル" withExtension:@"pdf"];
     PDFDocument *document = [[PDFDocument alloc] initWithURL:documentURL];
     self.pdfView.document = document;
+
+    [self statusLog];
+    [self updateViews];
 }
 
 - (IBAction)pushNext:(id)sender {
     [self.pdfView goToNextPage:nil];
+    [self statusLog];
+    [self updateViews];
 }
 
 - (IBAction)pushPrev:(id)sender {
     [self.pdfView goToPreviousPage:nil];
+    [self statusLog];
+    [self updateViews];
+}
+
+- (void)updateViews {
+    self.nextButton.enabled = self.pdfView.canGoToNextPage;
+    self.prevButton.enabled = self.pdfView.canGoToPreviousPage;
+}
+
+- (void)statusLog {
+    NSLog(@"canGoToFirstPage: %@", @(self.pdfView.canGoToFirstPage));
+    NSLog(@"canGoToLastPage: %@", @(self.pdfView.canGoToLastPage));
+    NSLog(@"canGoToNextPage: %@", @(self.pdfView.canGoToNextPage));
+    NSLog(@"canGoToPreviousPage: %@", @(self.pdfView.canGoToPreviousPage));
+}
 
 @end
